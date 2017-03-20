2f5f7a5512543fcc1070d02f9e5bf19434f279eb
sixiaoyou <yunshaliying@sina.com>
2017-03-20 21:31:16 +0800

    Update

---
+++ src/main/java/pers/you/learning/leetcode/three/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,18 @@
+package pers.you.learning.leetcode.three;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums,int size){
+        if(nums.length<size+1){
+            return nums.length;
+        }
+        int j=size,k=size;
+        while(j<nums.length){
+            if(nums[j]!=nums[k-size]){
+                nums[k]=nums[j];
+                k+=1;
+            }
+            j+=1;
+        }
+        return k;
+    }
+}
---
+++ src/main/java/pers/you/learning/leetcode/three/RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,17 @@
+package pers.you.learning.leetcode.three;
+
+public class RemoveDuplicatesFromSortedArrayII {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length<3){
+            return nums.length;
+        }
+        
+        int j=2,k=2;  
+        while(j<nums.length){  
+            if(nums[j]!=nums[k-2])  
+                nums[k++]=nums[j];  
+            j++;  
+        }  
+        return k;
+    }
+}
