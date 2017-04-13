2e7a3723af9e1a63061e8f6cda03d8cd953806f4
sixiaoyou <yunshaliying@sina.com>
2017-04-13 09:54:30 +0800

    Add LeetCode172-Factorial Trailing Zeroes

---
+++ src/main/java/pers/you/learning/leetcode/seventeenfour/FactorialTrailingZeroes.java
@@ -0,0 +1,22 @@
+package pers.you.learning.leetcode.seventeenfour;
+
+public class FactorialTrailingZeroes {
+    public int trailingZeroesV1(int n) {
+            int x=5;
+            int sum=0;
+            while(x<=n){
+                sum+=n/x;
+                x*=5;
+            }
+            return sum;
+    }
+    
+    public int trailingZeroesV2(int n) {
+        int log =(int)(Math.log(n)/Math.log(5));
+        int sum=0;
+        for(int i=1;i<log+1;i++){
+            sum+=n/(int)Math.pow(5, i);
+        }
+        return sum;
+    }
+}
