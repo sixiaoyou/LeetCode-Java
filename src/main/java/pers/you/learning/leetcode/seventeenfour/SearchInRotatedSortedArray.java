e25efb99bc4d0d2fa93f886494bbe51f9b5ac6ac
sixiaoyou <yunshaliying@sina.com>
2017-04-17 13:42:35 +0800

    Add LeetCode33:Search in Rotated Sorted Array

---
+++ src/main/java/pers/you/learning/leetcode/seventeenfour/SearchInRotatedSortedArray.java
@@ -0,0 +1,52 @@
+package pers.you.learning.leetcode.seventeenfour;
+
+/*
+LeetCode33:Search in Rotated Sorted Array
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+ */
+
+
+
+import org.junit.Ignore;
+
+public class SearchInRotatedSortedArray {
+    public int searchV1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int searchV2(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
