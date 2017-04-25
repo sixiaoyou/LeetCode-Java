95a4517a460462dcc0fd746af3d7c1ad36c7dd1c
sixiaoyou <yunshaliying@sina.com>
2017-04-25 08:20:15 +0800

    Update

--- src/main/java/pers/you/learning/leetcode/seventeenfour/RotateAnArrayOfNElementsToTheRightByKSteps.java
+++ src/main/java/pers/you/learning/leetcode/seventeenfour/RotateAnArrayOfNElementsToTheRightByKSteps.java
@@ -1,5 +1,21 @@
 package pers.you.learning.leetcode.seventeenfour;
 
+/*
+【极客学院版】
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+Note:
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+[show hint]
+
+Related problem: Reverse Words in a String II
+ */
+
+
+
 public class RotateAnArrayOfNElementsToTheRightByKSteps {
     public void swap(int[] array,int a,int b){
         int t = array[a];
