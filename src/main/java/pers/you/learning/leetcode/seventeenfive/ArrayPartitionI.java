720d24634afd5ae59a107ac59ef3598b26114a33
sixiaoyou <yunshaliying@sina.com>
2017-05-11 17:33:34 +0800

    Add LeetCode561:Array Partition I

---
+++ src/main/java/pers/you/learning/leetcode/seventeenfive/ArrayPartitionI.java
@@ -0,0 +1,30 @@
+/*
+LeetCode561:Array Partition I
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+Example 1:
+Input: [1,4,3,2]
+
+Output: 4
+Explanation: n is 2, and the maximum sum of pairs is 4.
+Note:
+n is a positive integer, which is in the range of [1, 10000].
+All the integers in the array will be in the range of [-10000, 10000].
+ */
+
+package pers.you.learning.leetcode.seventeenfive;
+
+import java.util.Arrays;
+
+public class ArrayPartitionI {
+   public int arrayPairSum(int[] nums) {
+       int sum=0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(i%2==0){
+                sum+=nums[i];
+            }
+        }
+        return sum;
+    }
+}
