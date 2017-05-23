214f2e67ed14391e9e168d79d76034f5400529f0
sixiaoyou <yunshaliying@sina.com>
2017-05-23 21:06:42 +0800

    Add LeetCode19-Remove Nth Node From End of List

--- src/main/java/pers/you/learning/leetcode/seventeenfive/ListNode.java
+++ src/main/java/pers/you/learning/leetcode/seventeenfive/ListNode.java
@@ -14,7 +14,17 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
-
+    
+    public  static ListNode arrayToList(int[] array) {
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        for (int value : array) {
+            p.next = new ListNode(value);
+            p = p.next;
+        }
+        return head.next;
+    }
+    
     @Override
     public String toString() {
         return "ListNode [val=" + val + "]";
