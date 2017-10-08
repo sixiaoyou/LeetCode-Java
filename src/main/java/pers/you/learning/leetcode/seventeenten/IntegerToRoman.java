3c46739120fef21d634ce4bf927cc54bf8bbb9b3
sixiaoyou <yunshaliying@sina.com>
2017-10-08 08:30:20 +0800

    Init

---
+++ src/main/java/pers/you/learning/leetcode/seventeenten/IntegerToRoman.java
@@ -0,0 +1,21 @@
+package pers.you.learning.leetcode.seventeenten;
+
+
+/*
+ '''
+【网友实现】https://discuss.leetcode.com/topic/32333/share-my-python-solution-96ms
+LeetCode12. Integer to Roman
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+'''
+ */
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        String[] M = {"", "M", "MM", "MMM"};
+        String[] C = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] X = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] I = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
+}
