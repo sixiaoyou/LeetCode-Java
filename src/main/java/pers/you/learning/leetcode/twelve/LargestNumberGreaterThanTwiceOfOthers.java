0242bdc64a7d989f8d9919ed62e11cf234ac0ecb
sixiaoyou <yunshaliying@sina.com>
2017-12-30 15:04:28 +0800

    Init

---
+++ src/main/java/pers/you/learning/leetcode/twelve/LargestNumberGreaterThanTwiceOfOthers.java
@@ -0,0 +1,49 @@
+package pers.you.learning.leetcode.twelve;
+/*
+'''
+LeetCode747. Largest Number Greater Than Twice of Others
+In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+
+Example 1:
+Input: nums = [3, 6, 1, 0]
+Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+Example 2:
+Input: nums = [1, 2, 3, 4]
+Output: -1
+Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+Note:
+nums will have a length in the range [1, 50].
+Every nums[i] will be an integer in the range [0, 99].
+'''
+ */
+
+public class LargestNumberGreaterThanTwiceOfOthers {
+    public int[] getMax(int[] nums) {
+        int[] max = { -Integer.MAX_VALUE, -1 };
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max[0]) {
+                max[0] = nums[i];
+                max[1] = i;
+
+            }
+        }
+        return max;
+    }
+
+    public int dominantIndex(int[] nums) {
+        int[] max = getMax(nums);
+        boolean mark = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (max[0] != nums[i] && max[0] < 2 * nums[i]) {
+                return -1;
+            }
+        }
+        return max[1];
+    }
+}
