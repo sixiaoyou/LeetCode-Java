c45f16238bd01385830f8d70d30d10336f260c1c
baichenhao <834628301@qq.com>
2021-02-24 21:39:54 +0800

    Add 1768. Merge Strings Alternately

--- src/main/java/pers/you/learning/leetcode/eighteen/AATest.java
+++ src/main/java/pers/you/learning/leetcode/eighteen/AATest.java
@@ -3,27 +3,24 @@ package pers.you.learning.leetcode.eighteen;
 import org.junit.Test;
 
 public class AATest { 
-	
-	public static void main(String[] args) {
-		String word1 = "abc";
-		String word2 = "pqr";
-		StringBuilder sb = new StringBuilder();
-		int l1 = word1.length();
-		int l2 = word2.length();
-		int min = Math.min(l1, l2);
-		for(int i=0;i<min;i++){
-			sb.append(word1.charAt(i));
-			sb.append(word2.charAt(i));
-		}
-		if(l1>l2){
-			for(int i=l2;i<l1;i++){
-				sb.append(word1.charAt(i));
-			}
-		}else if(l1<l2){
-			for(int i=l1;i<l2;i++){
-				sb.append(word2.charAt(i));
-			}
-		}
-		System.out.println(sb.toString());
-	}
+		 public String mergeAlternately(String word1, String word2) {
+				StringBuilder sb = new StringBuilder();
+				int l1 = word1.length();
+				int l2 = word2.length();
+				int min = Math.min(l1, l2);
+				for(int i=0;i<min;i++){
+					sb.append(word1.charAt(i));
+					sb.append(word2.charAt(i));
+				}
+				if(l1>l2){
+					for(int i=l2;i<l1;i++){
+						sb.append(word1.charAt(i));
+					}
+				}else if(l1<l2){
+					for(int i=l1;i<l2;i++){
+						sb.append(word2.charAt(i));
+					}
+				}
+				return(sb.toString());
+		    }
 }
---
+++ src/main/java/pers/you/learning/leetcode/eighteen/MergeStringsAlternately.java
@@ -0,0 +1,24 @@
+package pers.you.learning.leetcode.eighteen;
+
+public class MergeStringsAlternately {
+	 public String mergeAlternately(String word1, String word2) {
+			StringBuilder sb = new StringBuilder();
+			int l1 = word1.length();
+			int l2 = word2.length();
+			int min = Math.min(l1, l2);
+			for(int i=0;i<min;i++){
+				sb.append(word1.charAt(i));
+				sb.append(word2.charAt(i));
+			}
+			if(l1>l2){
+				for(int i=l2;i<l1;i++){
+					sb.append(word1.charAt(i));
+				}
+			}else if(l1<l2){
+				for(int i=l1;i<l2;i++){
+					sb.append(word2.charAt(i));
+				}
+			}
+			return(sb.toString());
+	    }
+}
