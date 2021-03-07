7993dd3ce40452c23b155d8b1d1c46de9f9060cd
baichenhao <834628301@qq.com>
2021-03-07 17:49:07 +0800

    Add FindPrime

---
+++ src/main/java/pers/you/learning/leetcode/eighteen/FindPrime.java
@@ -0,0 +1,36 @@
+package pers.you.learning.leetcode.eighteen;
+
+/**
+ * Description
+ *	输出n以内所有的素数。	
+ *	保证 n 是100以内的整数。
+ * 
+ */
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindPrime {
+	public boolean isPrime(int n) {
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public List<Integer> prime(int n) {
+		List<Integer> l = new ArrayList<Integer>();
+		for (int i = 2; i < n + 1; i++) {
+			if (isPrime(i)) {
+				l.add(i);
+			}
+		}
+		return l;
+		// write your code here
+	}
+
+}
