76a969a9ba87e9e7d8237548b9d9ae5b948df04b
baichenhao <834628301@qq.com>
2021-04-10 18:59:36 +0800

    Add LeetCode-1720. Decode XORed Array

--- src/main/java/pers/you/learning/leetcode/march/DecodeXORedArray.java
+++ src/main/java/pers/you/learning/leetcode/march/DecodeXORedArray.java
@@ -35,7 +35,6 @@ encoded.length == n - 1
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
-
 public class DecodeXORedArray {
     public int[] decode(int[] encoded, int first) {
         int[] res = new int[encoded.length+1];
