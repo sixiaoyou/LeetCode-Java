75a8d7b25f6ba11de6fe2c1a6551df0238ab4c19
baichenhao <834628301@qq.com>
2021-04-10 19:00:15 +0800

    Update

--- src/main/java/pers/you/learning/leetcode/march/DecodeXORedArray.java
+++ src/main/java/pers/you/learning/leetcode/march/DecodeXORedArray.java
@@ -35,6 +35,7 @@ encoded.length == n - 1
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
+
 public class DecodeXORedArray {
     public int[] decode(int[] encoded, int first) {
         int[] res = new int[encoded.length+1];
