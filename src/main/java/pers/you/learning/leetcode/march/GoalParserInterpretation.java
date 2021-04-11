a45d2a4ced4e5a99341a24ac02644d1391d32e34
baichenhao <834628301@qq.com>
2021-04-11 17:26:44 +0800

    Update

--- src/main/java/pers/you/learning/leetcode/march/GoalParserInterpretation.java
+++ src/main/java/pers/you/learning/leetcode/march/GoalParserInterpretation.java
@@ -35,6 +35,7 @@ command 由 "G"、"()" 和/或 "(al)" 按某种顺序组成
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
+
 public class GoalParserInterpretation {
     public String interpret(String command) {
         StringBuilder sb = new StringBuilder();
