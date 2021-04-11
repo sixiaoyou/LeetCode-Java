ccc29301dede089d2c2f5adab79e6ed61909434c
baichenhao <834628301@qq.com>
2021-04-11 17:22:45 +0800

    Add Two Files.

---
+++ src/main/java/pers/you/learning/leetcode/march/GoalParserInterpretation.java
@@ -0,0 +1,59 @@
+package pers.you.learning.leetcode.march;
+
+
+/*
+LeetCode 1678. 设计 Goal 解析器
+请你设计一个可以解释字符串 command 的 Goal 解析器 。command 由 "G"、"()" 和/或 "(al)" 按某种顺序组成。Goal 解析器会将 "G" 解释为字符串 "G"、"()" 解释为字符串 "o" ，"(al)" 解释为字符串 "al" 。然后，按原顺序将经解释得到的字符串连接成一个字符串。
+
+给你字符串 command ，返回 Goal 解析器 对 command 的解释结果。
+示例 1：
+
+输入：command = "G()(al)"
+输出："Goal"
+解释：Goal 解析器解释命令的步骤如下所示：
+G -> G
+() -> o
+(al) -> al
+最后连接得到的结果是 "Goal"
+示例 2：
+
+输入：command = "G()()()()(al)"
+输出："Gooooal"
+示例 3：
+
+输入：command = "(al)G(al)()()G"
+输出："alGalooG"
+ 
+
+提示：
+
+1 <= command.length <= 100
+command 由 "G"、"()" 和/或 "(al)" 按某种顺序组成
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/goal-parser-interpretation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class GoalParserInterpretation {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        int start=0,end=1;
+        while(start<command.length()){
+            if(command.charAt(start)=='G'){
+                sb.append("G");
+                start+=1;
+                end+=1;
+            }else if(command.charAt(start)=='('&&command.charAt(end)==')'){
+                sb.append("o");
+                start+=2;
+                end+=2;
+            }else{
+                sb.append("al");
+                start+=4;
+                end+=4;
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/pers/you/learning/leetcode/march/SingleRowKeyboard.java
@@ -0,0 +1,92 @@
+package pers.you.learning.leetcode.march;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+/*
+LeetCode 1165. 单行键盘
+我们定制了一款特殊的力扣键盘，所有的键都排列在一行上。
+
+我们可以按从左到右的顺序，用一个长度为 26 的字符串 keyboard （索引从 0 开始，到 25 结束）来表示该键盘的键位布局。
+
+现在需要测试这个键盘是否能够有效工作，那么我们就需要个机械手来测试这个键盘。
+
+最初的时候，机械手位于左边起第一个键（也就是索引为 0 的键）的上方。当机械手移动到某一字符所在的键位时，就会在终端上输出该字符。
+
+机械手从索引 i 移动到索引 j 所需要的时间是 |i - j|。
+
+当前测试需要你使用机械手输出指定的单词 word，请你编写一个函数来计算机械手输出该单词所需的时间。
+
+ 
+
+示例 1：
+
+输入：keyboard = "abcdefghijklmnopqrstuvwxyz", word = "cba"
+输出：4
+解释：
+机械手从 0 号键移动到 2 号键来输出 'c'，又移动到 1 号键来输出 'b'，接着移动到 0 号键来输出 'a'。
+总用时 = 2 + 1 + 1 = 4. 
+示例 2：
+
+输入：keyboard = "pqrstuvwxyzabcdefghijklmno", word = "leetcode"
+输出：73
+ 
+
+提示：
+
+keyboard.length == 26
+keyboard 按某种特定顺序排列，并包含每个小写英文字母一次。
+1 <= word.length <= 10^4
+word[i] 是一个小写英文字母
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/single-row-keyboard
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+
+
+public class SingleRowKeyboard {
+
+	String keyboard = "pqrstuvwxyzabcdefghijklmno";
+			
+	String word = "leetcode";
+	
+	
+	//Method One
+    public int calculateTime1(String keyboard, String word) {
+        Map<Character, Integer> map = new HashMap<Character,Integer>();
+    	for(int i=0;i<keyboard.length();i++){
+    		map.put(keyboard.charAt(i), i);
+    	}
+    	
+    	int time = map.get(word.charAt(0));
+        for(int i=0;i<word.length()-1;i++){
+        	time+=Math.abs(map.get(word.charAt(i))-map.get(word.charAt(i+1)));
+        }
+        
+        return time;
+    }
+//    Method Two
+    public int calculateTime2(String keyboard, String word) {
+    	char[] cw=word.toCharArray();
+    	int index=0,time=0;
+        for(char c:cw){
+        	time+=Math.abs(keyboard.indexOf(c)-index);
+        	index=keyboard.indexOf(c);
+        }
+        
+        return time;
+    }
+    
+    
+	@Test
+	public void test(){
+		System.out.println(calculateTime2(keyboard, word));
+	}
+	
+	
+	
+}
