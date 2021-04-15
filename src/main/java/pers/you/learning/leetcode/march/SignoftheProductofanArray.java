283bf69a422fcbd862b6a0a9165f3b5ce276428f
baichenhao <834628301@qq.com>
2021-04-15 16:42:46 +0800

    Update

--- src/main/java/pers/you/learning/leetcode/march/SignoftheProductofanArray.java
+++ src/main/java/pers/you/learning/leetcode/march/SignoftheProductofanArray.java
@@ -35,7 +35,6 @@ LeetCode1822. 数组元素积的符号
 链接：https://leetcode-cn.com/problems/sign-of-the-product-of-an-array
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-
 public class SignoftheProductofanArray {
     public int arraySign(int[] nums) {
         int a=0;
